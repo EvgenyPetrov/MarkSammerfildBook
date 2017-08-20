777387faadb9fe257263d165ab79c38509786313
Evgeny Petrov <petrov.evgeny.d@gmail.com>
2017-08-20 17:25:59 +0700

    chapter 7 example 1

--- src/filter/filter.go
+++ src/filter/filter.go
@@ -4,6 +4,9 @@ import (
     "flag"
     "log"
     "strings"
+    "path/filepath"
+    "os"
+    "fmt"
 )
 
 func main() {
@@ -28,4 +31,61 @@ func handleCommandLine() ( minSize, maxSize int64, suffixes, files []string) {
     }
     files = flag.Args()
     return  minSize, maxSize, suffixes, files
+}
+func source(files []string) <-chan string {
+    out := make(chan string, 1000)
+    go func() {
+        for _, filename := range files {
+            out <- filename
+        }
+        close(out)
+    }()
+    return out
+}
+func filterSuffixes(suffixes []string, in <-chan string) <-chan string {
+    out := make(chan string, cap(in))
+    go func() {
+        for filename := range in {
+            if len(suffixes) == 0 {
+                out <- filename
+                continue
+            }
+            ext := strings.ToLower(filepath.Ext(filename))
+            for _, suffix := range suffixes {
+                if ext == suffix {
+                    out <- filename
+                    break
+                }
+            }
+        }
+        close(out)
+    }()
+    return out
+}
+func filterSize(minimum, maximum int64, in <-chan string) <-chan string {
+    out := make(chan string, cap(in))
+    go func() {
+        for filename := range in {
+            if minimum == -1 && maximum == -1 {
+                out <- filename // don't do a stat call it not needed
+                continue
+            }
+            finfo, err := os.Stat(filename)
+            if err != nil {
+                continue // ignore files we can't process
+            }
+            size := finfo.Size()
+            if (minimum == -1 || minimum > -1 && minimum <= size) &&
+                (maximum == -1 || maximum > -1 && maximum >= size) {
+                out <- filename
+            }
+        }
+        close(out)
+    }()
+    return out
+}
+func sink(in <-chan string) {
+    for filename := range in {
+        fmt.Println(filename)
+    }
 }
